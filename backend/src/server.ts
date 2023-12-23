5a89092e0180100ee1fae0ae745ec7a0a42f6017
Ennygenius <inuenike@gmail.com>
2023-12-23 11:16:12 +0100

    commited

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,9 +19,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://krestonbank.netlify.app/"
+    "https://krestonbank.netlify.app"
   );
-  // res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
