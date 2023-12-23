5a6f091ff487293f9ee89cb8bf908d2eb83e6e28
Ennygenius <inuenike@gmail.com>
2023-12-23 03:22:20 +0100

    commited

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 2005;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https:127.0.0.1:5173",
+  })
+);
+app.options("*", cors());
 app.use(cookieParser());
 
 app.use("/users", UserRouter);
