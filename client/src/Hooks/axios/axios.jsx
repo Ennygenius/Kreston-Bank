a1e4c5a253c402193b89cebe8cbf7efaf4b9bdb5
Ennygenius <inuenike@gmail.com>
2023-12-23 02:42:01 +0100

    commited

--- client/src/Hooks/axios/axios.jsx
+++ client/src/Hooks/axios/axios.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const BASE = axios.create({
-  // baseURL: "http://127.0.0.1:3005/",
-  baseURL: "https://krestonbank.onrender.com/",
+  baseURL: "http://127.0.0.1:3005/",
 });
